d08836b0cf50cf5a3a1196582d4249dabbe66e83
Stualyttle <you@example.com>
2026-04-12 15:10:13 +0200

    Add HeroSection component and styles for homepage layout

---
+++ app/(home)/components/HeroSection/index.tsx
@@ -0,0 +1,27 @@
+import { Button, Heading, Text } from "@lyttle-development/ui";
+import styles from "./index.module.scss";
+
+export function HeroSection() {
+  return (
+    <section id="home" className={styles.heroShell}>
+      <article className={styles.heroContent}>
+        <Heading as="h1" size="6xl" className={styles.heroTitle}>
+          Mealz on Wheelz
+        </Heading>
+        <Text as="p" size="lg" className={styles.heroText}>
+          Mealz on Wheelz is jouw go-to foodtruck in het Oost-Vlaamse
+          Moerbeke-Waas. Wij bieden concepten aan zoals pasta's, BBQ en nog
+          meer. Kom langs of vraag een offerte en ontdek het zelf!
+        </Text>
+        <div className={styles.heroActions}>
+          <Button asChild variant="brand" size="lg">
+            <a href="#reserveren">Boek ons</a>
+          </Button>
+          <Button asChild variant="secondary" size="lg">
+            <a href="#formules">Ontdek menu</a>
+          </Button>
+        </div>
+      </article>
+    </section>
+  );
+}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,13 @@
-import { AboutSection } from './components/AboutSection';
-import { BookingSection } from './components/BookingSection';
-import { ContactSection } from './components/ContactSection';
-import { Footer } from './components/Footer';
-import { FormulasSection } from './components/FormulasSection';
-import { GallerySection } from './components/GallerySection';
-import { Header } from './components/Header';
-import { HeroSection } from './components/HeroSection';
-import { ServicesSection } from './components/ServicesSection';
-import styles from './page.module.scss';
+import { AboutSection } from "./components/AboutSection";
+import { BookingSection } from "./components/BookingSection";
+import { ContactSection } from "./components/ContactSection";
+import { Footer } from "./components/Footer";
+import { FormulasSection } from "./components/FormulasSection";
+import { GallerySection } from "./components/GallerySection";
+import { Header } from "./components/Header";
+import { HeroSection } from "./components/HeroSection";
+import { ServicesSection } from "./components/ServicesSection";
+import styles from "./page.module.scss";
 
 export default function HomePage() {
   return (
@@ -24,4 +24,3 @@ export default function HomePage() {
     </main>
   );
 }
-
